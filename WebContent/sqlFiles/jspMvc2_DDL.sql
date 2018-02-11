f72dfae7452371a13c5f3512d6076b6d8fc0adf3
gonnichiwa <keepthefaith00@gmail.com>
2018-02-11 21:40:39 +0900

    sqlFile추가

---
+++ WebContent/sqlFiles/jspMvc2_DDL.sql
@@ -0,0 +1,30 @@
+
+desc board;
+desc reply;
+
+drop table board;
+drop table reply;
+
+create table board(
+  num int primary key,
+  name varchar2(20) not null,
+  password varchar2(20) not null,
+  subject varchar2(20) not null,
+  content varchar2(2000) not null,
+  write_date varchar2(20) not null,
+  write_time varchar2(20) not null,
+  ref int not null,
+  step int not null,
+  lev int not null,
+  read_cnt int not null,
+  child_cnt int not null
+);
+
+create table reply(
+  rpy_num int primary key,
+  rpy_author varchar2(50) not null,
+  rpy_content varchar2(400) not null,
+  rpy_date varchar2(70) not null,
+  rpy_parent_num int not null
+);
+
